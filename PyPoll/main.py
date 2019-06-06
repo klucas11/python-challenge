231500bd4f9c5a7cf3568356f8fbc037368c9e90
Kristin Lucas <klucas11@aggienetwork.com>
2019-06-05 22:41:42 -0500

    Print percentages

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,18 @@
 import os
 import csv
 
-csvpath = os.path.join('election_data.csv')
 
 # Variables
+csvpath = os.path.join('election_data.csv')
 total_votes = 0
 candidate = 0
 candidate_dict = {}
 
+
+def calculate_percent_of_votes(total_votes, candidate):
+    value = candidate / total_votes
+    percent_of_votes = value * 100
+    return percent_of_votes
 
 # Open CSV
 with open(csvpath, 'r', newline="") as csvfile:
@@ -16,20 +21,21 @@ with open(csvpath, 'r', newline="") as csvfile:
     csv_header = next(election_data)
 
     for row in election_data:
+
         candidate_name = row[2]
 
         if candidate_name in candidate_dict:
             candidate_dict[candidate_name] = candidate_dict[candidate_name] + 1
-        else:
+        else: 
             candidate_dict[candidate_name] = 1
 
-
         # calculate total votes
-        # total_votes = total_votes + 1
+        total_votes = total_votes + 1
 
     print(candidate_dict)
 
-
+    for candidate in candidate_dict:
+        print(calculate_percent_of_votes(total_votes, candidate_dict[candidate]))
 
 # # Summary Table
 # print("Election Results")
